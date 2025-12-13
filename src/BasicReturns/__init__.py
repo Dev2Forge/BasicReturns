d45af73fb2ed5f33515de1ea476714c86155e29f
Tutos Rive <108991712+tutosrive@users.noreply.github.com>
2025-12-13 16:43:20 -0500

    Upgrading module, first basic version

---
+++ src/BasicReturns/__init__.py
@@ -0,0 +1,7 @@
+from .main import BasicReturn, DataAndMsgReturn
+
+__author__ = 'tutosrive'
+__org__ = 'Dev2Forge'
+__web_page__ = 'https://pypi.org/project/BasicReturns'
+__doc__ = 'All functions ever return the same models with particulars properties'
+__all__ = ['BasicReturn', 'DataAndMsgReturn']
--- src/BasicReturns/main.py
+++ src/BasicReturns/main.py
@@ -5,8 +5,16 @@ All functions ever return the same models with particulars properties
 from dataclasses import dataclass
 from typing import Any, Optional
 
-@dataclass
-class BasicsReturns:
+@dataclass(kw_only=True)
+class BasicReturn:
+    """Have a basic return model, contains `ok` and `error` properties.
+
+    Attributes
+        `ok: bool`: Similar to HTTP response, this means if something execution was successfully or no
+        `error: Any`: When `ok` is `False`, this contains the error, like an exception or other
+    
+    Methods
+        **to_dict()**"""
     ok: Optional[bool] = True
     error: Optional[Any] = None
 
@@ -14,7 +22,24 @@ class BasicsReturns:
         return f'OK: {self.ok}\nError: {self.error}'
     
     def to_dict(self):
-        return{
-            "ok": self.ok,
-            "error": self.error
-        }
+        """Get a self representation as dictionary"""
+        return{"ok": self.ok, "error": self.error}
+
+@dataclass(kw_only=True)
+class DataAndMsgReturn(BasicReturn):
+    """Get a object return with basic properties (`ok`, `error`) and `msg` and `data` properties
+    
+    Attrubutes:
+        `msg: str`: A message to provide information about an operation executed
+        `data: Any`: A object that contains any data type, means that method was executed successfully
+    
+    Methods
+        **to_dict()**"""
+    msg: Optional[str] = None
+    data: Optional[Any] = None
+
+    def __str__(self):
+        return f'{super().__str__()}\nMessage: {self.msg}\nData: {self.data}\nError: {self.error}'
+    
+    def to_dict(self):
+        return{**super().to_dict(), "msg": self.msg, "data": self.data if self.data else {}}
