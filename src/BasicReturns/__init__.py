def3c2de8d71f03c843c5370297138cfda53d1b3
Tutos Rive <108991712+tutosrive@users.noreply.github.com>
2025-12-13 17:38:40 -0500

    Upgrade module to first try upload pypi

--- src/BasicReturns/__init__.py
+++ src/BasicReturns/__init__.py
@@ -1,7 +1,7 @@
 from .main import BasicReturn, DataAndMsgReturn
 
-__author__ = 'tutosrive'
-__org__ = 'Dev2Forge'
-__web_page__ = 'https://pypi.org/project/BasicReturns'
-__doc__ = 'All functions ever return the same models with particulars properties'
-__all__ = ['BasicReturn', 'DataAndMsgReturn']
+__author__: str = 'tutosrive'
+__org__: str = 'Dev2Forge'
+__web_page__: str = 'https://pypi.org/project/BasicReturns'
+__doc__: str = 'All functions ever return the same models with particulars properties'
+__all__: list[str] = ['BasicReturn', 'DataAndMsgReturn']
